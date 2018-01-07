4cf05dfaa1d85fdd461782910174632716fc6a2e
Takanori Suzuki <mail.tks@gmail.com>
2018-01-08 03:07:22 +0900

    Implement TaskController class and file logger feature

--- blank_detector.py
+++ blank_detector.py
@@ -2,7 +2,11 @@
 
 import ctypes
 import struct
-from logging import getLogger, StreamHandler, Formatter, WARN, INFO, DEBUG
+from logging import getLogger, StreamHandler, FileHandler, Formatter, WARN, INFO, DEBUG
+import subprocess
+import select
+import multiprocessing
+import time
 
 logger = getLogger(__name__)
 formatter = Formatter('%(asctime)s - %(levelname)s: %(message)s',
@@ -12,7 +16,6 @@ handler.setFormatter(formatter)
 handler.setLevel(INFO)
 logger.setLevel(INFO)
 logger.addHandler(handler)
-logger.propagate = False
 
 ctypes.cdll.LoadLibrary('libXext.so')
 libXext = ctypes.CDLL('libXext.so')
@@ -47,22 +50,80 @@ def get_DPMS_state(display_name_in_byte_string=b':0'):
     return state
 
 
-class TaskManager(object):
-    def __init__(self, cmd):
-        pass
+class TaskController(object):
+    STREAM_TYPE_STDOUT = 'stdout'
+    STREAM_TYPE_STDERR = 'stderr'
+
+    def __init__(self,
+                 cmd,
+                 func_for_stdout=None,
+                 func_for_stderr=None):
+        self.__is_running = False
+        self.cmd = cmd
+        if func_for_stdout is None:
+            self.func_for_stdout = self.default_func_for_stream
+        else:
+            self.func_for_stdout = func_for_stdout
+        if func_for_stderr is None:
+            self.func_for_stderr = self.default_func_for_stream
+        else:
+            self.func_for_stderr = func_for_stderr
+
+    def default_func_for_stream(self, cmd_output, stream_type):
+        if cmd_output == '':
+            return
+        elif stream_type == self.STREAM_TYPE_STDOUT:
+            logger.info('STDOUT: ' + cmd_output.rstrip('\r\n'))
+        elif stream_type == self.STREAM_TYPE_STDERR:
+            logger.warning('STDERR: ' + cmd_output.rstrip('\r\n'))
+
+    def _worker(self):
+        proc = subprocess.Popen(self.cmd,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                shell=True, universal_newlines=True)
+
+        def getfd(s):
+            return s.fileno()
+        while True:
+            reads = [getfd(proc.stdout), getfd(proc.stderr)]
+            ret = select.select(reads, [], [])
+
+            for fd in ret[0]:
+                if fd == getfd(proc.stdout):
+                    read = proc.stdout.readline()
+                    self.func_for_stdout(read, self.STREAM_TYPE_STDOUT)
+                if fd == getfd(proc.stderr):
+                    read = proc.stderr.readline()
+                    self.func_for_stderr(read, self.STREAM_TYPE_STDERR)
+
+            if proc.poll() is not None:
+                break
 
     def is_running(self):
-        return 1
+        return self.__is_running
 
     def start(self):
-        logger.info('Start process')
+        logger.info('Starting \'%s\'...' % self.cmd)
+        if self.__is_running is False:
+            self.__process = multiprocessing.Process(target=self._worker,
+                                                     args=[])
+            self.__process.start()
+            self.__is_running = True
+            logger.info('Started.')
+        else:
+            logger.info('Already started. Nothing to do.')
 
     def stop(self):
-        logger.info('Stop process')
+        logger.info('Stopping \'%s\'...' % self.cmd)
+        if self.__is_running is True:
+            self.__process.terminate()
+            self.__is_running = False
+            logger.info('Stopped.')
+        else:
+            logger.info('Already stopped. Nothing to do.')
 
 
 def main():
-    import time
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-d',
@@ -70,36 +131,56 @@ def main():
                         action='store',
                         required=True,
                         help='Specify display name in format \':0.0\'')
+    parser.add_argument('-c',
+                        '--command',
+                        action='store',
+                        required=True,
+                        help='Set a command to execute')
+    parser.add_argument('-l',
+                        '--log',
+                        action='store',
+                        help='Set log file path for logging')
     args = parser.parse_args()
-    logger.info('\'%s\' started' % __file__)
+    if args.log:
+        file_handler = FileHandler(args.log)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(INFO)
+        logger.addHandler(file_handler)
+
+    logger.info('############# \'%s\' started #############' % __file__)
     display_name_in_byte_string = args.display.encode('ascii')
     dpms_state = DPMSFAIL
-    task = TaskManager('./non_stop_cmd.sh') # should be changed
-    while True:
-        new_dpms_state = get_DPMS_state(display_name_in_byte_string)
-        if dpms_state != new_dpms_state:
-            dpms_state = new_dpms_state
-            if dpms_state == DPMSFAIL:
-                logger.info('DPMS state is detected as [DPMSFAIL]')
-                if task.is_running():
-                    task.stop()
-            elif dpms_state == DPMSModeOn:
-                logger.info('DPMS state is detected as [DPMSModeOn]')
-                if task.is_running():
-                    task.stop()
-            elif dpms_state == DPMSModeStandby:
-                logger.info('DPMS state is detected as [DPMSModeStandby]')
-                if not task.is_running():
-                    task.start()
-            elif dpms_state == DPMSModeSuspend:
-                logger.info('DPMS state is detected as [DPMSModeSuspend]')
-                if not task.is_running():
-                    task.start()
-            elif dpms_state == DPMSModeOff:
-                logger.info('DPMS state is detected as [DPMSModeOff]')
-                if not task.is_running():
-                    task.start()
-        time.sleep(1)
+    task = TaskController(args.command)
+    try:
+        while True:
+            new_dpms_state = get_DPMS_state(display_name_in_byte_string)
+            if dpms_state != new_dpms_state:
+                dpms_state = new_dpms_state
+                if dpms_state == DPMSFAIL:
+                    logger.info('DPMS state of \'%s\' is detected as [DPMSFAIL]' % args.display)
+                    if task.is_running():
+                        task.stop()
+                elif dpms_state == DPMSModeOn:
+                    logger.info('DPMS state of \'%s\' is detected as [DPMSModeOn]' % args.display)
+                    if task.is_running():
+                        task.stop()
+                elif dpms_state == DPMSModeStandby:
+                    logger.info('DPMS state of \'%s\' is detected as [DPMSModeStandby]' % args.display)
+                    if not task.is_running():
+                        task.start()
+                elif dpms_state == DPMSModeSuspend:
+                    logger.info('DPMS state of \'%s\' is detected as [DPMSModeSuspend]' % args.display)
+                    if not task.is_running():
+                        task.start()
+                elif dpms_state == DPMSModeOff:
+                    logger.info('DPMS state of \'%s\' is detected as [DPMSModeOff]' % args.display)
+                    if not task.is_running():
+                        task.start()
+            time.sleep(1)
+    finally:
+        if task.is_running():
+            task.stop()
+        logger.info('############# \'%s\' terminating #############' % __file__)
 
 if __name__ == '__main__':
     main()
